f8a422c119ba79de45d41ef624c1f7eaafde5301
Michael Dellapi <mad758@nyu.edu>
2018-01-27 17:38:03 -0500

    Ch-ch-changes for the slot machine
    Slot machine animation can loop forever

--- Unity/TransmissionGame/Assets/Scripts/spinningSlot.cs
+++ Unity/TransmissionGame/Assets/Scripts/spinningSlot.cs
@@ -3,17 +3,21 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 public class spinningSlot : MonoBehaviour {
-	public Text slotText1;
+	public Text slotText1;//the actual yext
 	public Text slotText2;
 	public Text slotText3;
 	public Text slotText4;
 	public Text slotText5;
 	public Text slotText6;
 
-	public Image[] slot;
-	public Sprite slot1Blur;
-	public Image handle;
-	public Sprite handleDown;
+	public Image[] slot;//the image inside the canvas that simulates spinning
+	public Sprite originalSlot;//non-spinning slot chamber
+	public Sprite[] slot1Blur;//spinning slot chamber
+	public Image handle;//slot machine handle canvas image
+	public Sprite handleUp;//slot machine sprite in neutral 
+	public Sprite handleDown;//slot machine sprite pushed down
+
+	int randomSlot;
 
 	// Use this for initialization
 	void Start () {
@@ -22,7 +26,7 @@ public class spinningSlot : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetKeyDown (KeyCode.Space)) {
+		if (Input.GetKey (KeyCode.Space)) {
 			slotText1.text = "";
 			slotText2.text = "";
 			slotText3.text = "";
@@ -30,15 +34,34 @@ public class spinningSlot : MonoBehaviour {
 			slotText5.text = "";
 			slotText6.text = "";
 
-			slot[0].sprite = slot1Blur;
-			slot [1].sprite = slot1Blur;
-			slot[2].sprite = slot1Blur;
-			slot [3].sprite = slot1Blur;
-			slot[4].sprite = slot1Blur;
-			slot [5].sprite = slot1Blur;
+			randomSlot = Random.Range (0, 3);
+			Debug.Log (randomSlot);
+			slot[0].sprite = slot1Blur[randomSlot];
+			slot [1].sprite = slot1Blur[randomSlot];
+			slot[2].sprite = slot1Blur[randomSlot];
+			slot [3].sprite = slot1Blur[randomSlot];
+			slot[4].sprite = slot1Blur[randomSlot];
+			slot [5].sprite = slot1Blur[randomSlot];
 
-			handle.transform.position = new Vector2 (handle.transform.position.x, -10);
+			//handle.transform.position = new Vector2 (handle.transform.position.x, -1);
 			handle.sprite = handleDown;
+		}
+		if (Input.GetKeyUp (KeyCode.Space)) {
+			slotText1.text = "You";
+			slotText2.text = "never";
+			slotText3.text = "digested";
+			slotText4.text = "my";
+			slotText5.text = "ass";
+			slotText6.text = "?";
+
+			slot [0].sprite = originalSlot;
+			slot [1].sprite = originalSlot;
+			slot [2].sprite = originalSlot;
+			slot [3].sprite = originalSlot;
+			slot [4].sprite = originalSlot;
+			slot [5].sprite = originalSlot;
+
+			handle.sprite = handleUp;
 		}
 	}
 }
