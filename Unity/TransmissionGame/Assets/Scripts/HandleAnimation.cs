28c633d016d43f86f15014c38be148e2c326ce2c
Unknown <katesuzannah@gmail.com>
2018-01-28 10:54:32 -0500

    Public functions, fixing compiler errors

---
+++ Unity/TransmissionGame/Assets/Scripts/HandleAnimation.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HandleAnimation : MonoBehaviour {
+	public Image handle;//slot machine handle canvas image
+	public Sprite handleUp;//slot machine sprite in neutral 
+	public Sprite handleDown;//slot machine sprite pushed down
+	Vector2 handleUpPos;
+
+	void Start () {
+		handleUpPos = handle.transform.localPosition;
+	}
+
+	public void PullHandle () {
+		handle.sprite = handleDown;
+		handle.transform.localPosition = new Vector2 (handle.transform.localPosition.x, handle.transform.localPosition.y - 55f);
+	}
+
+	public void ReleaseHandle () {
+		handle.sprite = handleUp;
+		handle.transform.localPosition = handleUpPos;
+	}
+}
